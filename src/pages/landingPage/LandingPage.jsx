0927d8deebd478feaf5aeebba9dc01a07281ad18
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2022-06-27 21:39:48 -0700

    publishing website initiation

---
+++ src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './landingPage.css'
+import { Navbar } from '../../components'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  )
+}
+
+export default LandingPage
